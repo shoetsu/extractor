86acc50576e6fac131510debd557975eec987fa4
shoetsu <shoetsu@recruit.ai>
2018-01-17 16:06:26 -0800

    update .gitignore

--- stanford-postagger.sh
+++ stanford-postagger.sh
@@ -1,12 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 #
 # usage: ./stanford-postagger.sh model textFile
 #  e.g., ./stanford-postagger.sh models/english-left3words-distsim.tagger sample-input.txt
 
+usage() {
+    echo "Usage:$0 input_file"
+    exit 1
+}
+
+if [ $# -lt 1 ];then
+    usage;
+fi
+input_path=$1
 ROOT_DIR=/home/shoetsu/downloads/stanford-postagger
 MODEL_PATH=$ROOT_DIR/models/english-left3words-distsim.tagger
 #java -mx300m -cp $ROOT_DIR/stanford-postagger.jar: edu.stanford.nlp.tagger.maxent.MaxentTagger -model $MODEL_PATH -textFile $1 -outputFormat tsv -sentenceDelimiter newline -tokenize false > $1.tagged
-java -mx2g -cp $ROOT_DIR/stanford-postagger.jar: edu.stanford.nlp.tagger.maxent.MaxentTagger -model $MODEL_PATH -textFile $1 -outputFormat tsv -sentenceDelimiter newline -tokenize false > $1.tagged
+java -mx2g -cp $ROOT_DIR/stanford-postagger.jar: edu.stanford.nlp.tagger.maxent.MaxentTagger -model $MODEL_PATH -textFile $input_path -outputFormat tsv -sentenceDelimiter newline -tokenize false > $input_path.tagged
 
 #echo "java -mx300m -cp $ROOT_DIR/stanford-postagger.jar: edu.stanford.nlp.tagger.maxent.MaxentTagger -model $MODEL_PATH -textFile $1 -outputFormat tsv -sentenceDelimiter newline -tokenize false > $1.tagged"
 
